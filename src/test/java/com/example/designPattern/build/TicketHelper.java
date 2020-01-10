28647d070f8ce7c96f97528bab1d8f06ea14827d
wql <weiqinlei@jajaying.com>
2020-01-10 16:58:32 +0800

    建造者模式

---
+++ src/test/java/com/example/designPattern/build/TicketHelper.java
@@ -0,0 +1,24 @@
+package com.example.designPattern.build;
+
+public class TicketHelper {
+
+    public void buildAdult (String info) {
+        System.err . println ( "构建成年人票逻辑：" + info);
+    }
+
+    public void buildBaby (String info) {
+        System.err . println ( "构建儿童无座票：" + info);
+    }
+
+    public void buildChild (String info) {
+        System.err . println ( "构建儿童有座票：" + info);
+    }
+
+    public void buildOld (String info) {
+        System.err . println ( "构建老人票：" + info);
+    }
+
+    public void buildSoldier (String info) {
+        System.err . println ( "构建军人票：" + info);
+    }
+}
